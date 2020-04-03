9d4d78881ab17650cd440ca4ca18877f7b8dfba0
Massimo Da Ros <massimo.daros@bitrock.it>
2020-04-03 21:20:00 +0200

    Initial commit

---
+++ src/test/java/it/bitrock/training/cucumber/feature/authentication/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package it.bitrock.training.cucumber.feature.authentication;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith ( Cucumber.class )
+@CucumberOptions ( plugin = { "pretty" } )
+public class RunCucumberTest {
+
+}
